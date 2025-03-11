37a9af8c294ff810f1f1ffa044a49addf0087f6e
otaviocode <luizgomesdossantos1970@gmail.com>
2025-03-11 17:12:15 -0300

    Primeiro commit - projeto criado

---
+++ thera_sync/src/main/java/com/thera/sync/TheraSyncApplication.java
@@ -0,0 +1,13 @@
+package com.thera.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheraSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheraSyncApplication.class, args);
+	}
+
+}
---
+++ thera_sync/src/test/java/com/thera/sync/TheraSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thera.sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheraSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
